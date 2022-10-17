f001fe475b7c0882f867a3aaf2dc09367b4e43f5
imhilla <hillaryodhiambo282@gmail.com>
2022-10-17 05:42:30 +0300

    update app

--- LinkedList/list.py
+++ LinkedList/list.py
@@ -31,21 +31,42 @@ while current:
 # appending to the end of the list
 
 
+# class SinglyLinkedList:
+#     def __init__(self):
+#         self.head = None
+#         self.size = 0
+
+#     def append(self, data):
+#         # encapsulate the data in a Node
+#         node = Node(data)
+#         if self.head is None:
+#             self.head = node
+#         else:
+#             current = self.head
+#             while current.next:
+#                 current = current.next
+#             current.next = node
+
+#     def iter(self):
+#         current = self.head
+#         while current:
+#             val = current.data
+#             current = current.next
+#             yield val
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
         self.size = 0
 
     def append(self, data):
-        # encapsulate the data in a Node
         node = Node(data)
-        if self.head is None:
-            self.head = node
+        if self.tail:
+            self.tail.next = node
+            self.tail = node
         else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = node
+            self.head = node
+            self.tail = node
 
     def iter(self):
         current = self.head
@@ -62,3 +83,5 @@ words.append("spam")
 
 for word in words.iter():
     print(word)
+
+# append at intermediate positions
